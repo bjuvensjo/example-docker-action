cac5eb017bdfa204c770b332da72fd0c6a44f719
Magnus Bjuvensjö <magnus.bjuvensjo@tietoevry.com>
2024-12-02 09:06:29 +0100

    modified:   entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,9 @@
 
 set -ex
 
-INPUT_GITHUB_TOKEN=$1
-REPOSITORY_OWNER=bjuvensjo
-REPOSITORY=slask
-BRANCH=main
-REMOTE_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY_OWNER}/${REPOSITORY}.git"
+# INPUT_GITHUB_TOKEN=$1
+INPUT_GITHUB_TOKEN=${ACTIONS_RUNTIME_TOKEN}
+REMOTE_REPO="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 WORKSPACE=/app/workspace
 
 env
@@ -14,13 +12,13 @@ env
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]" 
 
-mkdir $WORKSPACE
-git clone ${REMOTE_REPO} $WORKSPACE/${REPOSITORY}
+mkdir -p $WORKSPACE/${GITHUB_REPOSITORY}
+git clone ${REMOTE_REPO} $WORKSPACE/${GITHUB_REPOSITORY}
 
 cd /app
-uv run src/example_docker_action/example.py $WORKSPACE/${REPOSITORY}/foo.txt
+uv run src/example_docker_action/example.py $WORKSPACE/${GITHUB_REPOSITORY}/foo.txt
 
-cd $WORKSPACE/${REPOSITORY}
+cd $WORKSPACE/${GITHUB_REPOSITORY}
 git add foo.txt
 git commit -m "Updated foo.txt"
-git push "${REMOTE_REPO}" HEAD:${BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+git push "${REMOTE_REPO}" HEAD:${GITHUB_REF_NAME} --follow-tags $_FORCE_OPTION $_TAGS;
